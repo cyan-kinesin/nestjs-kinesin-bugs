33e0f4e8cc6d2bc31fc73ff3b2d3284d65013fb0
Cyan Kinesin <4thston@gmail.com>
2021-09-21 05:30:52 +0900

    add fastify adapter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // with express
+  //   const app = await NestFactory.create(AppModule);
+
+  // with fastify
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   app.enableVersioning({
     type: VersioningType.HEADER,
